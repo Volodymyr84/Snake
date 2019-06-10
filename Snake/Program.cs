1696334f840924bcbae6d96380643ddbf25187c5
Volodymyr84 <38746456+Volodymyr84@users.noreply.github.com>
2019-06-10 14:39:10 +0300

    test with List

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -15,15 +15,16 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
+            Point p3 = new Point(3, 3, '/');
 
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
             numList.Add(2);
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+            int x = numList[2];
+            int y = numList[0];
+            int z = numList[1];
 
             foreach (int i in numList)
             {
@@ -35,6 +36,10 @@ namespace Snake
             List<Point> pList = new List<Point>();
             pList.Add(p1);
             pList.Add(p2);
+            pList.Add(p3);
+
+            pList.Remove(p1);
+            pList.RemoveAt(1);
 
             Console.ReadLine();
         }
