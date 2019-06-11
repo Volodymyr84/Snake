30bee50dd3fd913d94c267f415516ab7d3d495df
Volodymyr84 <38746456+Volodymyr84@users.noreply.github.com>
2019-06-11 14:31:28 +0300

    Lesson_11.1

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -32,14 +32,7 @@ namespace Snake
                 if (Console.KeyAvailable)
                 { 
                 ConsoleKeyInfo key = Console.ReadKey();
-                if (key.Key == ConsoleKey.LeftArrow)
-                    snake.direction = Direction.LEFT;
-                else if (key.Key == ConsoleKey.RightArrow)
-                    snake.direction = Direction.RIGHT;
-                else if (key.Key == ConsoleKey.UpArrow)
-                    snake.direction = Direction.UP;
-                else if (key.Key == ConsoleKey.DownArrow)
-                    snake.direction = Direction.DOWN;
+                    snake.HandleKey(key.Key);
                 }
 
                 Thread.Sleep(100);
